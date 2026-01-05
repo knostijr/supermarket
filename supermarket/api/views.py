e5ba0db33cf8e81d476319c9699860d4b1b10029
knostijr <christophkonst@icloud.com>
2026-01-05 14:04:42 +0100

    GET and Post Requests

--- supermarket/api/views.py
+++ supermarket/api/views.py
@@ -1,6 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
-@api_view()
+@api_view(['GET', 'POST'])
 def first_view(request):
-    return Response({"message": "Hello, world!"})
+    if request.method == 'GET':
+        return Response({"message": "Hello, world!"})
+    if request.method == 'POST':
+        try:
+            msg = request.data['message']
+            return Response({"your_message": msg}, status=status.HTTP_201_CREATED)
+        except:    
+            return Response({"your_message": "error"},status=status.HTTP_400_BAD_REQUEST)
+        
+
+            
