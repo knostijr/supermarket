89fc928ca5dcb4617a027e837130cf1e39a6780f
knostijr <christophkonst@icloud.com>
2026-01-05 16:01:40 +0100

    serializer DELETE

--- supermarket/api/views.py
+++ supermarket/api/views.py
@@ -4,14 +4,14 @@ from rest_framework import status
 from .serializers import MarketSerializer
 from supermarket.models import Market
 
+
 @api_view(['GET', 'POST'])
 def markets_view(request):
     if request.method == 'GET':
         markets = Market.objects.all()
         serializer = MarketSerializer(markets, many=True)
         return Response(serializer.data)
-    
-    
+
     if request.method == 'POST':
         serializer = MarketSerializer(data=request.data)
         if serializer.is_valid():
@@ -19,6 +19,28 @@ def markets_view(request):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
-        
 
-            
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def market_single_view(request, pk):
+    if request.method == 'GET':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+        
+    
+    
+    if request.method == 'DELETE':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market)
+        market.delete()
+        return Response(serializer.data)
