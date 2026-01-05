391272fd93238ed9c9c9800d50f670e5f5c7007d
knostijr <christophkonst@icloud.com>
2026-01-05 14:42:21 +0100

    first serializer

---
+++ supermarket/api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+#Er wandelt Django-Objekte (z. B. Model-Instanzen) in JSON um, 
+#damit sie über eine API ausgeliefert werden können.
+
+#Der Serializer prüft:
+#Datentypen
+#Pflichtfelder
+#Längen
+#Eigene Geschäftslogik
+
+class CommentSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255)
+    location = serializers.CharField(max_length=255)
+    description = serializers.CharField()
+    net_worth = serializers.DecimalField(max_digits=100, decimal_places=2)
